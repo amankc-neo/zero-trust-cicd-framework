f2083927293dcd92d3a52aa4047103d66e7ea5b9
Aman Choudhary <hoonwangdu@gmail.com>
2025-06-17 11:19:23 +0530

    vault config push

--- azure/vault/vault-role-create.sh
+++ azure/vault/vault-role-create.sh
@@ -6,3 +6,7 @@ vault write auth/jwt/role/github-actions \
   bound_subject="spiffe://azure/github/org/repo/azure-deploy" \
   policies="github-actions" \
   ttl="15m"
+  
+# Binds a specific SPIFFE ID to a Vault role
+# Grants access to specific secrets or mounts via a policy
+# Limits tokens to 15 minutes
